70e6202dc8a89813f9f297d0ac883595ca616fc1
zhaizhonghao <390930230@qq.com>
2020-09-15 10:55:15 +0800

    add a persistence

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,7 +1,16 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 type Block struct {
@@ -35,4 +44,36 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	block.Hash = hash[:]
 	block.Nonce = nonce
 	return block
+}
+
+//serialize the block
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	//An Encoder manages the transmission of type and data information to the other side of a connection.
+	//It is safe for concurrent use by multiple goroutines.
+	encoder := gob.NewEncoder(&res)
+	//translate the block into bytes in gob
+	err := encoder.Encode(b)
+	Handle(err)
+
+	return res.Bytes()
+}
+
+//deserialize the bytes to the block
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	Handle(err)
+
+	return &block
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
